c4edfb7ebaf20e7cf0419576a0ee5d2e68dc1a58
Richard Hao <ithaoxilu@gmail.com>
2017-11-25 18:23:31 +0530

    inital dotfiles-local

---
+++ zsh/configs/post/oh-my-zsh.zsh
@@ -0,0 +1,17 @@
+export ZSH=/Users/haoxilu/.oh-my-zsh
+
+ZSH_THEME="robbyrussell"
+
+plugins=(
+  git
+  rails
+  ruby
+  bundler
+  docker
+  git-flow
+  osx
+)
+
+source $ZSH/oh-my-zsh.sh
+
+export ARCHFLAGS="-arch x86_64"
