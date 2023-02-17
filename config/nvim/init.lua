939b00f29aa0e8f38a300aa53ac72a6e1fac75f3
Richard Hao <richard@digitalstar.tech>
2023-02-17 22:40:26 +0800

    feat: add neovim support for vscode

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -7,43 +7,26 @@ if vim.g.neovide then
   vim.api.nvim_set_keymap('!', '<D-v>', '<C-R>+', { noremap = true, silent = true})
   vim.api.nvim_set_keymap('t', '<D-v>', '<C-R>+', { noremap = true, silent = true})
   vim.api.nvim_set_keymap('v', '<D-v>', '<C-R>+', { noremap = true, silent = true})
+elseif vim.g.vscode then
+  -- VSCode extension
+  vim.api.nvim_set_keymap('x', 'gc', '<Plug>VSCodeCommentary', { noremap = false, silent = true})
+  vim.api.nvim_set_keymap('n', 'gc', '<Plug>VSCodeCommentary', { noremap = false, silent = true})
+  vim.api.nvim_set_keymap('o', 'gc', '<Plug>VSCodeCommentary', { noremap = false, silent = true})
+  vim.api.nvim_set_keymap('n', 'gcc', '<Plug>VSCodeCommentaryLine', { noremap = false, silent = true})
+
+  --clipboard
+  if vim.fn.has("clipboard") then
+    if vim.fn.has("unnamedplus") then
+      -- When possible use + register for copy-paste
+      vim.opt.clipboard = { "unnamed", "unnamedplus" }
+    else
+      -- On mac and Windows, use * register for copy-paste
+      vim.opt.clipboard = { "unnamed" }
+    end
+  end
 else
   vim.opt.runtimepath:prepend('~/.vim')
   vim.opt.runtimepath:append('~/.vim/after')
   vim.g.python3_host_prog = '/usr/local/bin/python3'
   vim.cmd('source ~/.vimrc')
 end
-
---
--- if exists('g:vscode')
---   " VSCode extension
---   xmap gc  <Plug>VSCodeCommentary
---   nmap gc  <Plug>VSCodeCommentary
---   omap gc  <Plug>VSCodeCommentary
---   nmap gcc <Plug>VSCodeCommentaryLine
-
---   "clipboard
---   if has("clipboard")
---     if has("unnamedplus")
---       " When possible use + register for copy-paste
---       set clipboard=unnamed,unnamedplus
---     else
---       " On mac and Windows, use * register for copy-paste
---       set clipboard=unnamed
---     endif
---   endif
--- elseif exists("g:neovide")
---   set guifont= "JetBrainsMono Nerd Font:h13"
-
---   let g:neovide_input_use_logo = 1
---   map <D-v> "+p<CR>
---   map! <D-v> <C-R>+
---   tmap <D-v> <C-R>+
---   vmap <D-c> "+y<CR> 
--- else
---   " ordinary Neovim
---   set runtimepath^=~/.vim runtimepath+=~/.vim/after
---   let &packpath = &runtimepath
---   let g:python3_host_prog="/usr/local/bin/python3"
---   source ~/.vimrc
--- endif
