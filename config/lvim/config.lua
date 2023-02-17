26f44838f517cf7cd96fe78a4f95d26e4cb2d70b
Richard Hao <richard.hao@crypto.com>
2023-02-17 16:41:40 +0800

    feat: add neovide keybindings

--- config/lvim/config.lua
+++ config/lvim/config.lua
@@ -228,3 +228,19 @@ lvim.plugins = {
     end
   },
 }
+
+if vim.g.neovide then
+  vim.keymap.set('n', '<D-s>', ':w<CR>') -- Save
+  vim.keymap.set('v', '<D-c>', '"+y') -- Copy
+  vim.keymap.set('n', '<D-v>', '"+P') -- Paste normal mode
+  vim.keymap.set('v', '<D-v>', '"+P') -- Paste visual mode
+  vim.keymap.set('c', '<D-v>', '<C-R>+') -- Paste command mode
+  vim.keymap.set('i', '<D-v>', '<ESC>l"+Pli') -- Paste insert mode
+  vim.api.nvim_set_option("clipboard", "unnamed")
+
+  vim.g.neovide_remember_window_size = true
+  vim.g.neovide_remember_window_position = true
+  vim.g.neovide_input_use_logo = 1
+  vim.g.neovide_input_macos_alt_is_meta = true
+  vim.opt.title = true
+end
