709f4807260d9d9e681168b1718af70d84104f6f
Richard Hao <richard@digitalstar.tech>
2023-02-17 23:16:34 +0800

    feat: add neovide options

--- config/lvim/config.lua
+++ config/lvim/config.lua
@@ -311,5 +311,7 @@ if vim.g.neovide then
   vim.g.neovide_input_macos_alt_is_meta = true
   vim.g.neovide_confirm_quit = true
   vim.g.neovide_cursor_vfx_mode = "railgun"
+  vim.g.neovide_hide_mouse_when_typing = true
   vim.opt.title = true
+  vim.opt.titlestring = "%<%F - 0x"
 end
