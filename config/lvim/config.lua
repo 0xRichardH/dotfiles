3e0ae5bc93db7e412c58b6e895bdbb2ce6bd4a68
Richard Hao <richard@digitalstar.tech>
2023-02-18 10:52:16 +0800

    feat: update keybindings

--- config/lvim/config.lua
+++ config/lvim/config.lua
@@ -33,8 +33,10 @@ lvim.builtin.alpha.dashboard.section.header.val = {
 lvim.leader = "space"
 -- add your own keymapping
 lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
-lvim.keys.normal_mode["<S-l>"] = ":BufferLineCycleNext<CR>"
-lvim.keys.normal_mode["<S-h>"] = ":BufferLineCyclePrev<CR>"
+vim.api.nvim_set_keymap("n", "<TAB>", ":bnext<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<S-TAB>", ":bprevious<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<S-l>", ":bnext<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<S-h>", ":bprevious<CR>", { noremap = true, silent = true })
 lvim.builtin.which_key.mappings["z"] = {
   "<cmd>ZenMode<CR>:set nospell<CR>", "Zen Mode"
 }
