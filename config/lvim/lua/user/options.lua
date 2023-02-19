49bed8e6d8a9643a57b4bd0156d3c24325dd6ab9
Richard Hao <richard@digitalstar.tech>
2023-02-20 05:11:25 +0800

    refactor: move nvim config to lua

---
+++ config/lvim/lua/user/options.lua
@@ -0,0 +1,45 @@
+-- general
+lvim.log.level = "warn"
+lvim.format_on_save.enabled = true
+lvim.colorscheme = "ayu-light"
+lvim.use_icons = true
+vim.opt.guifont = "Hack:h16"
+vim.g.italic_comments = true -- italic comments(Default: true)
+vim.g.italic_keywords = true -- italic keywords(Default: true)
+vim.g.italic_functions = true -- italic functions(Default: false)
+vim.g.italic_variables = true -- italic variables(Default: false)
+
+-- Shorter ASCII art logo, so not too much space is taken up.
+lvim.builtin.alpha.dashboard.section.header.val = {
+  "▌              ▌ ▌▗",
+  "▌  ▌ ▌▛▀▖▝▀▖▙▀▖▚▗▘▄ ▛▚▀▖",
+  "▌  ▌ ▌▌ ▌▞▀▌▌  ▝▞ ▐ ▌▐ ▌",
+  "▀▀▘▝▀▘▘ ▘▝▀▘▘   ▘ ▀▘▘▝ ▘",
+}
+
+lvim.keys.term_mode = { ["<C-l>"] = false }
+lvim.builtin.cmp.cmdline.enable = false
+lvim.builtin.alpha.active = true
+lvim.builtin.alpha.mode = "dashboard"
+lvim.builtin.terminal.active = true
+lvim.builtin.nvimtree.setup.view.side = "left"
+lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
+
+-- if you don't want all the parsers change this to a table of the ones you want
+lvim.builtin.treesitter.ensure_installed = {
+  "bash",
+  "c",
+  "javascript",
+  "json",
+  "lua",
+  "python",
+  "typescript",
+  "tsx",
+  "css",
+  "rust",
+  "java",
+  "yaml",
+}
+
+lvim.builtin.treesitter.ignore_install = { "haskell" }
+lvim.builtin.treesitter.highlight.enable = true
