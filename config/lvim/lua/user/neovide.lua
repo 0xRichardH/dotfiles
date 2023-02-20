65ecec35823263dffb6bedcff1b6483acac5066a
Richard Hao <richard@digitalstar.tech>
2023-02-20 21:45:53 +0800

    fix: fix copy & paste in neovide (#24)

--- config/lvim/lua/user/neovide.lua
+++ config/lvim/lua/user/neovide.lua
@@ -1,12 +1,10 @@
 -- neovide
 if vim.g.neovide then
-  vim.keymap.set('n', '<D-s>', ':w<CR>') -- Save
-  vim.keymap.set('v', '<D-c>', '"+y') -- Copy
-  vim.keymap.set('n', '<D-v>', '"+P') -- Paste normal mode
-  vim.keymap.set('v', '<D-v>', '"+P') -- Paste visual mode
-  vim.keymap.set('c', '<D-v>', '<C-R>+') -- Paste command mode
-  vim.keymap.set('i', '<D-v>', '<ESC>l"+Pli') -- Paste insert mode
   vim.api.nvim_set_option("clipboard", "unnamed")
+  vim.api.nvim_set_keymap('', '<D-v>', '+p<CR>', { noremap = true, silent = true })
+  vim.api.nvim_set_keymap('!', '<D-v>', '<C-R>+', { noremap = true, silent = true })
+  vim.api.nvim_set_keymap('t', '<D-v>', '<C-R>+', { noremap = true, silent = true })
+  vim.api.nvim_set_keymap('v', '<D-v>', '<C-R>+', { noremap = true, silent = true })
 
   vim.g.neovide_remember_window_size = true
   vim.g.neovide_remember_window_position = true
