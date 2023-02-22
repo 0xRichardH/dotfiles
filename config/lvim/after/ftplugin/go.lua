9c35da9c92e84eb0629534b1a4033e431039b99a
Richard Hao <89900155+richard-cryptodotcom@users.noreply.github.com>
2023-02-22 22:38:13 +0800

    feat: Add gofumpt, goimports and golangci-lint supports (#26)

---
+++ config/lvim/after/ftplugin/go.lua
@@ -0,0 +1,11 @@
+local formatters = require "lvim.lsp.null-ls.formatters"
+formatters.setup {
+  { name = "gofumpt",   filetypes = { "go" } },
+  { name = "goimports", args = { "-srcdir", "$DIRNAME" }, filetypes = { "go" } },
+}
+
+local linters = require "lvim.lsp.null-ls.linters"
+linters.setup {
+  -- golangci-lint
+  { name = "golangci_lint", args = { "run", "--fix=false", "--out-format=json", "--path-prefix", "$ROOT" }, filetypes = { "go" } },
+}
