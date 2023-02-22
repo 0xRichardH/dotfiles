ad3db3c73e379a9a6389983766a32c4b6048755a
Richard Hao <richard@digitalstar.tech>
2023-02-23 01:10:55 +0800

    feat: update 2023-02-22 (#27)
    * feat: add go lsp keybindings
    
    * feat: add typescript/javascript keymaps
    
    * feat: enable relative number
    
    * feat: update 2023-02-22

--- config/lvim/after/ftplugin/go.lua
+++ config/lvim/after/ftplugin/go.lua
@@ -1,9 +1,11 @@
+-- formatters
 local formatters = require "lvim.lsp.null-ls.formatters"
 formatters.setup {
   { name = "gofumpt",   filetypes = { "go" } },
   { name = "goimports", args = { "-srcdir", "$DIRNAME" }, filetypes = { "go" } },
 }
 
+-- linters
 local linters = require "lvim.lsp.null-ls.linters"
 linters.setup {
   -- golangci-lint
