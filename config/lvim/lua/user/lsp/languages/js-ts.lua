929f28b3e1805e49136cd8e1e91067a914a9b9aa
Richard Hao <richard@digitalstar.tech>
2023-02-23 21:16:25 +0800

    feat: add lsp for css, js-ts, sh

---
+++ config/lvim/lua/user/lsp/languages/js-ts.lua
@@ -0,0 +1,42 @@
+-- Setup lsp.
+vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "tsserver" })
+
+local capabilities = require("lvim.lsp").common_capabilities()
+
+require("typescript").setup {
+  -- disable_commands = false, -- prevent the plugin from creating Vim commands
+  debug = false, -- enable debug logging for commands
+  go_to_source_definition = {
+    fallback = true, -- fall back to standard LSP definition on failure
+  },
+  server = { -- pass options to lspconfig's setup method
+    on_attach = require("lvim.lsp").common_on_attach,
+    on_init = require("lvim.lsp").common_on_init,
+    capabilities = capabilities,
+    settings = {
+      typescript = {
+        inlayHints = {
+          includeInlayEnumMemberValueHints = true,
+          includeInlayFunctionLikeReturnTypeHints = true,
+          includeInlayFunctionParameterTypeHints = false,
+          includeInlayParameterNameHints = "all", -- 'none' | 'literals' | 'all';
+          includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+          includeInlayPropertyDeclarationTypeHints = true,
+          includeInlayVariableTypeHints = true,
+        },
+      },
+    },
+  },
+}
+
+-- Set a formatter.
+local formatters = require "lvim.lsp.null-ls.formatters"
+formatters.setup {
+  { command = "prettier", filetypes = { "javascript", "typescript", "javascriptreact", "typescriptreact", "css" } },
+}
+
+-- Set a linter.
+local linters = require("lvim.lsp.null-ls.linters")
+linters.setup({
+  { command = "eslint", filetypes = { "javascript", "typescript" } },
+})
