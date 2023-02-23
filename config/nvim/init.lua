2dade6a3f0cd39a3ee69979622cadf6169edcbb9
Richard Hao <richard@digitalstar.tech>
2023-02-23 22:35:10 +0800

    chore: remove vim config

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -3,16 +3,16 @@ if vim.g.neovide then
 
   -- Allow clipboard copy paste in neovim
   vim.g.neovide_input_use_logo = true
-  vim.api.nvim_set_keymap('', '<D-v>', '+p<CR>', { noremap = true, silent = true})
-  vim.api.nvim_set_keymap('!', '<D-v>', '<C-R>+', { noremap = true, silent = true})
-  vim.api.nvim_set_keymap('t', '<D-v>', '<C-R>+', { noremap = true, silent = true})
-  vim.api.nvim_set_keymap('v', '<D-v>', '<C-R>+', { noremap = true, silent = true})
+  vim.api.nvim_set_keymap('', '<D-v>', '+p<CR>', { noremap = true, silent = true })
+  vim.api.nvim_set_keymap('!', '<D-v>', '<C-R>+', { noremap = true, silent = true })
+  vim.api.nvim_set_keymap('t', '<D-v>', '<C-R>+', { noremap = true, silent = true })
+  vim.api.nvim_set_keymap('v', '<D-v>', '<C-R>+', { noremap = true, silent = true })
 elseif vim.g.vscode then
   -- VSCode extension
-  vim.api.nvim_set_keymap('x', 'gc', '<Plug>VSCodeCommentary', { noremap = false, silent = true})
-  vim.api.nvim_set_keymap('n', 'gc', '<Plug>VSCodeCommentary', { noremap = false, silent = true})
-  vim.api.nvim_set_keymap('o', 'gc', '<Plug>VSCodeCommentary', { noremap = false, silent = true})
-  vim.api.nvim_set_keymap('n', 'gcc', '<Plug>VSCodeCommentaryLine', { noremap = false, silent = true})
+  vim.api.nvim_set_keymap('x', 'gc', '<Plug>VSCodeCommentary', { noremap = false, silent = true })
+  vim.api.nvim_set_keymap('n', 'gc', '<Plug>VSCodeCommentary', { noremap = false, silent = true })
+  vim.api.nvim_set_keymap('o', 'gc', '<Plug>VSCodeCommentary', { noremap = false, silent = true })
+  vim.api.nvim_set_keymap('n', 'gcc', '<Plug>VSCodeCommentaryLine', { noremap = false, silent = true })
 
   --clipboard
   if vim.fn.has("clipboard") then
@@ -25,8 +25,4 @@ elseif vim.g.vscode then
     end
   end
 else
-  vim.opt.runtimepath:prepend('~/.vim')
-  vim.opt.runtimepath:append('~/.vim/after')
-  vim.g.python3_host_prog = '/usr/local/bin/python3'
-  vim.cmd('source ~/.vimrc')
 end
