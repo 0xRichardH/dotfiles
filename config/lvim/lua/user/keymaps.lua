462a4189d4064893581ab1a042d1bcc6f4649935
Richard Hao <richard@digitalstar.tech>
2023-02-23 22:27:27 +0800

    feat: add lsp for css, js-ts, sh (#29)
    * feat: add lsp for css, js-ts, sh
    
    * feat: update lsp related config
    
    * feat: add cybu.nvim
    
    * chore: update default user name

--- config/lvim/lua/user/keymaps.lua
+++ config/lvim/lua/user/keymaps.lua
@@ -2,10 +2,7 @@
 lvim.leader = "space"
 -- add your own keymapping
 lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
-vim.api.nvim_set_keymap("n", "<TAB>", ":bnext<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<S-TAB>", ":bprevious<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<S-l>", ":bnext<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<S-h>", ":bprevious<CR>", { noremap = true, silent = true })
+
 lvim.builtin.which_key.mappings["z"] = {
   "<cmd>ZenMode<CR>:set nospell<CR>", "Zen Mode"
 }
--- config/lvim/lua/user/lsp/init.lua
+++ config/lvim/lua/user/lsp/init.lua
@@ -1,1 +1,20 @@
 require "user.lsp.languages.go"
+require "user.lsp.languages.css"
+require "user.lsp.languages.sh"
+require "user.lsp.languages.js-ts"
+
+-- if you don't want all the parsers change this to a table of the ones you want
+lvim.builtin.treesitter.ensure_installed = {
+  "bash",
+  "javascript",
+  "json",
+  "lua",
+  "typescript",
+  "tsx",
+  "css",
+  "yaml",
+  "go"
+}
+
+lvim.builtin.treesitter.ignore_install = { "haskell" }
+lvim.builtin.treesitter.highlight.enable = true
