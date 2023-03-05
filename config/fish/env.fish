b4beb7522b2df21ed5e9b23e26a58f2ea5da1176
Richard Hao <richard@digitalstar.tech>
2023-03-05 23:43:43 +0800

    feat(lf): add configs for lf file manager

--- config/fish/env.fish
+++ config/fish/env.fish
@@ -1,8 +1,8 @@
 set -gx fish_greeting # disable fish greeting
 set -gx PNPM_HOME "$HOME/Library/pnpm"
 
-set -Ux EDITOR vim 
-set -U VISUAL vim
+set -Ux EDITOR lvim 
+set -U VISUAL lvim
 set -U LANG en_US.UTF-8
 set -U LC_ALL en_US.UTF-8
 
