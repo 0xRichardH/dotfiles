223ddd84358d855480e8264b2254883d7df8ea74
Richard Hao <richard@digitalstar.tech>
2023-03-07 21:11:14 +0800

    chore: remove lsp related files

--- config/lvim/lua/user/lsp/init.lua
+++ config/lvim/lua/user/lsp/init.lua
@@ -1,8 +1,3 @@
-require "user.lsp.languages.go"
-require "user.lsp.languages.css"
-require "user.lsp.languages.sh"
-require "user.lsp.languages.js-ts"
-
 -- if you don't want all the parsers change this to a table of the ones you want
 lvim.builtin.treesitter.ensure_installed = {
   "bash",
