f7a4e30a3bd70b141fb97fb5abe94eeeaecfa509
Richard Hao <richard@digitalstar.tech>
2023-03-09 08:07:43 +0800

    feat(scripts): install lvim

--- install.sh
+++ install.sh
@@ -18,4 +18,10 @@ source "${dotfiles_path}/scripts/package-manager.sh"
 # shellcheck source=./scripts/brew.sh
 source "${dotfiles_path}/scripts/brew.sh"
 
+# install lunarvim
+LV_BRANCH='release-1.2/neovim-0.8' bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/fc6873809934917b470bff1b072171879899a36b/utils/installer/install.sh)
+
+# Update dotfiles
+rcup
+
 echo "Done."
