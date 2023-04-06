63b82e093c564999fe26f5cb922e56180688c026
Richard Hao <richard@digitalstar.tech>
2023-04-06 14:11:39 +0800

    chore: remove mrsk alias

--- config/fish/alias.fish
+++ config/fish/alias.fish
@@ -7,4 +7,3 @@ alias psql pgcli
 alias npm pnpm
 alias git hub
 alias v 'fd --type f --hidden --exclude .git | fzf-tmux -p --reverse | xargs lvim'
-alias mrsk='docker run --rm -it -v $HOME/.ssh:/root/.ssh -v /var/run/docker.sock:/var/run/docker.sock -v {$PWD}/:/workdir  ghcr.io/mrsked/mrsk'
