785b910d4027933e743c3e2baca651d157af732f
Richard Hao <richard@digitalstar.tech>
2023-05-07 12:06:03 +0800

    feat(lvim): add ray-x/go.nvim

--- config/lvim/lua/user/plugins.lua
+++ config/lvim/lua/user/plugins.lua
@@ -77,6 +77,22 @@ lvim.plugins = {
     --  { "<leader>ff", "<cmd>Lf<cr>", desc = "NeoTree" },
     -- }
   },
+
+  -- Modern Go dev plugin
+  {
+    "ray-x/go.nvim",
+    dependencies = { -- optional packages
+      "ray-x/guihua.lua",
+      "neovim/nvim-lspconfig",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    config = function()
+      require("go").setup()
+    end,
+    event = { "CmdlineEnter" },
+    ft = { "go", 'gomod' },
+    build = ':lua require("go.install").update_all_sync()' -- if you need to install/update all binaries
+  },
 }
 
 -- disable plugins
