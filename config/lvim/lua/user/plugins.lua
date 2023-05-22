b84c78c0cf3bb5d278409644db61daf0d82b6098
Richard Hao <richard@digitalstar.tech>
2023-05-22 10:17:46 +0800

    feat(lvim): add plugin hbac.nvim to autoclose buffers

--- config/lvim/lua/user/plugins.lua
+++ config/lvim/lua/user/plugins.lua
@@ -121,6 +121,21 @@ lvim.plugins = {
   {
     "ray-x/sad.nvim",
     config = sad_config.sad
+  },
+
+  -- Heuristic buffer auto-close
+  {
+    'axkirillov/hbac.nvim',
+    config = function()
+      require("hbac").setup({
+        autoclose = true, -- set autoclose to false if you want to close manually
+        threshold = 5,    -- hbac will start closing unedited buffers once that number is reached
+        close_command = function(bufnr)
+          vim.api.nvim_buf_delete(bufnr, {})
+        end,
+        close_buffers_with_windows = false, -- hbac will close buffers with associated windows if this option is `true`
+      })
+    end
   }
 }
 
