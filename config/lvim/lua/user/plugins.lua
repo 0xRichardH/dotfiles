9f9d87ea789ecbd9ba1c4e2d905720708c0f1310
Richard Hao <richard@digitalstar.tech>
2023-05-23 18:23:01 +0800

    feat(lvim): add plugin to preview markdown

--- config/lvim/lua/user/plugins.lua
+++ config/lvim/lua/user/plugins.lua
@@ -136,7 +136,10 @@ lvim.plugins = {
         close_buffers_with_windows = false, -- hbac will close buffers with associated windows if this option is `true`
       })
     end
-  }
+  },
+
+  -- A markdown preview directly in your neovim.
+  { "ellisonleao/glow.nvim", config = true, cmd = "Glow" }
 }
 
 -- disable plugins
