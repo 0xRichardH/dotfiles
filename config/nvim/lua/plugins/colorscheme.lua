48786bc9ad19a362a18825bf94a3239457ce38b1
Richard Hao <richard@digitalstar.tech>
2023-05-29 15:14:50 +0800

    feat(nvim): add plugins for golang development

--- config/nvim/lua/plugins/colorscheme.lua
+++ config/nvim/lua/plugins/colorscheme.lua
@@ -6,19 +6,6 @@ return {
     opts = {
       flavour = "mocha", -- latte, frappe, macchiato, or mocha
       transparent_background = true,
-      color_overrides = {
-        all = {
-          surface0 = "#444444",
-          surface1 = "#666666",
-          surface2 = "#a3a7bc",
-          surface3 = "#a3a7bc",
-        },
-      },
-      integrations = {
-        cmp = true,
-        gitsigns = true,
-        telescope = true,
-      },
     },
   },
   -- {
