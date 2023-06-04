ee8a1d8c8ad3ad635d74ec33e87a2c9a77879418
Richard Hao <richard@digitalstar.tech>
2023-06-04 13:28:28 +0800

    feat(nvim): add keybindings for moving lines with a pair of keymaps

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -2,13 +2,29 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-local map = vim.keymap.set
+local function map(mode, lhs, rhs, opts)
+  local keys = require("lazy.core.handler").handlers.keys
+  ---@cast keys LazyKeysHandler
+  -- do not create the keymap if a lazy keys handler exists
+  if not keys.active[keys.parse({ lhs, mode = mode }).id] then
+    opts = opts or {}
+    opts.silent = opts.silent ~= false
+    if opts.remap and not vim.g.vscode then
+      opts.remap = nil
+    end
+    vim.keymap.set(mode, lhs, rhs, opts)
+  end
+end
 
 -- map enter to ciw
 map("n", "<cr>", "ciw", { desc = "ciw" })
 
 -- add keymaps for
 map("n", "ga", "<cmd>CodeActionMenu<cr>", { desc = "CodeActionMenu" })
+
+-- move lines with a pair of keymaps
+map("v", "J", ":m '>+1<CR>gv=gv")
+map("v", "K", ":m '<-2<CR>gv=gv")
 
 -- custom keymaps
 map("n", "gl", vim.diagnostic.open_float, { desc = "Line Diagnostics" })
