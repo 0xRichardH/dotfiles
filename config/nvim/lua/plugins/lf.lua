e69ea5adc1c8bd6b9e8f757aa030d9fbbebf2c7d
Richard Hao <richard@digitalstar.tech>
2023-06-09 16:59:49 +0800

    fix(nvim): cannot open lf after updating the plugin

--- config/nvim/lua/plugins/lf.lua
+++ config/nvim/lua/plugins/lf.lua
@@ -1,14 +1,20 @@
 return {
-  "lmburns/lf.nvim",
+  "0x4richard/lf.nvim",
+  lazy = true,
   cmd = "Lf",
   dependencies = { "nvim-lua/plenary.nvim", "akinsho/toggleterm.nvim" },
   opts = {
+    default_cmd = "lf",
     winblend = 0,
     highlights = { NormalFloat = { guibg = "NONE" } },
+    direction = "float",
     border = "double", -- border kind: single double shadow curved
     height = 0.70,
     width = 0.85,
     escape_quit = true,
+    focus_on_open = false, -- focus the current file when opening Lf (experimental)
+    mappings = true, -- whether terminal buffer mapping is enabled
+    tmux = false, -- tmux statusline can be disabled on opening of Lf
   },
   keys = {
     { "<leader>o", "<cmd>Lf<cr>", desc = "File Manager" },
