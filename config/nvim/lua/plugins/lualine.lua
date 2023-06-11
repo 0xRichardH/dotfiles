e542a512870c7270a6018570d3573a191566ccd9
Richard Hao <richard@digitalstar.tech>
2023-06-12 00:55:08 +0800

    chore(nvim): display filename in the lualine

--- config/nvim/lua/plugins/lualine.lua
+++ config/nvim/lua/plugins/lualine.lua
@@ -52,7 +52,7 @@ return {
         lualine_b = {},
         lualine_c = {
           { "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
-          { "filename", path = 1, symbols = { modified = "  ", readonly = "", unnamed = "" } },
+          { "filename", path = 0, symbols = { modified = "  ", readonly = "", unnamed = "" } },
         },
         lualine_x = { diff, diagnostics },
         lualine_y = {},
