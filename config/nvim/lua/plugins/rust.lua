5aaccf38a3d7eefc2d97e8d0fd01a27971a66062
Richard Hao <richard@0xdev.dev>
2023-06-21 12:54:54 +0800

    feat(nvim): add rust-tools.nvim for the rust

---
+++ config/nvim/lua/plugins/rust.lua
@@ -0,0 +1,45 @@
+return {
+  {
+    "simrat39/rust-tools.nvim",
+    ft = "rust",
+    opts = {
+      reload_workspace_from_cargo_toml = true,
+      inlay_hints = {
+        enable = true,
+        -- automatically set inlay hints (type hints)
+        -- default: true
+        auto = true,
+
+        -- Only show inlay hints for the current line
+        only_current_line = false,
+
+        -- whether to show parameter hints with the inlay hints or not
+        -- default: true
+        show_parameter_hints = true,
+
+        -- prefix for parameter hints
+        -- default: "<-"
+        parameter_hints_prefix = "<- ",
+
+        -- prefix for all the other hints (type, chaining)
+        -- default: "=>"
+        other_hints_prefix = "=> ",
+
+        -- whether to align to the length of the longest line in the file
+        max_len_align = false,
+
+        -- padding from the left if max_len_align is true
+        max_len_align_padding = 1,
+
+        -- whether to align to the extreme right or not
+        right_align = false,
+
+        -- padding from the right if right_align is true
+        right_align_padding = 7,
+
+        -- The color of the hints
+        highlight = "Comment",
+      },
+    },
+  },
+}
