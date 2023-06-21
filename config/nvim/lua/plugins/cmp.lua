c12bc5a8ccee3f0e72b16ec10721681ff78061f0
Richard Hao <richard@0xdev.dev>
2023-06-21 13:11:02 +0800

    chore(nvim): remove unused code

--- config/nvim/lua/plugins/cmp.lua
+++ config/nvim/lua/plugins/cmp.lua
@@ -1,12 +1,5 @@
 return {
   {
-    "L3MON4D3/LuaSnip",
-    -- Disable default <tab> and <s-tab> behavior in LuaSnip
-    keys = function()
-      return {}
-    end,
-  },
-  {
     "hrsh7th/nvim-cmp",
     dependencies = {
       { "jcdickinson/codeium.nvim" },
@@ -39,38 +32,6 @@ return {
         end,
       }
       vim.api.nvim_set_hl(0, "CmpItemKindCodeium", { fg = "#6CC644" })
-
-      -- Supertab
-      -- local luasnip = require("luasnip")
-      -- local has_words_before = function()
-      --   unpack = unpack or table.unpack
-      --   local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-      --   return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-      -- end
-      -- opts.mapping = vim.tbl_extend("force", opts.mapping, {
-      --   ["<Tab>"] = cmp.mapping(function(fallback)
-      --     if cmp.visible() then
-      --       cmp.select_next_item()
-      --     -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
-      --     -- this way you will only jump inside the snippet region
-      --     elseif luasnip.expand_or_jumpable() then
-      --       luasnip.expand_or_jump()
-      --     elseif has_words_before() then
-      --       cmp.complete()
-      --     else
-      --       fallback()
-      --     end
-      --   end, { "i", "s" }),
-      --   ["<S-Tab>"] = cmp.mapping(function(fallback)
-      --     if cmp.visible() then
-      --       cmp.select_prev_item()
-      --     elseif luasnip.jumpable(-1) then
-      --       luasnip.jump(-1)
-      --     else
-      --       fallback()
-      --     end
-      --   end, { "i", "s" }),
-      -- })
     end,
   },
 }
