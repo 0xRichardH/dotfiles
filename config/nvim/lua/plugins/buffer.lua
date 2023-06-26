57bf83ef1f68a65e7935e26e88b3d9b9cd8761f1
Richard Hao <richard@0xdev.dev>
2023-06-27 00:36:27 +0800

    chore(nvim):  Remove the `ghillb/cybu.nvim` plugin

--- config/nvim/lua/plugins/buffer.lua
+++ config/nvim/lua/plugins/buffer.lua
@@ -13,49 +13,4 @@ return {
       })
     end,
   },
-  {
-    "ghillb/cybu.nvim",
-    event = "VeryLazy",
-    config = function()
-      local ok, cybu = pcall(require, "cybu")
-      if not ok then
-        return
-      end
-      cybu.setup({
-        position = {
-          relative_to = "win", -- win, editor, cursor
-          anchor = "topcenter", -- topleft, topcenter, topright,
-          -- centerleft, center, centerright,
-          -- bottomleft, bottomcenter, bottomright
-          -- vertical_offset = 10, -- vertical offset from anchor in lines
-          -- horizontal_offset = 0, -- vertical offset from anchor in columns
-          -- max_win_height = 5, -- height of cybu window in lines
-          -- max_win_width = 0.5, -- integer for absolute in columns
-          -- float for relative to win/editor width
-        },
-        display_time = 1750, -- time the cybu window is displayed
-        style = {
-          path = "relative", -- absolute, relative, tail (filename only)
-          path_abbreviation = "none", -- none, shortened
-          border = "rounded", -- single, double, rounded, none
-          separator = " ", -- string used as separator
-          prefix = "…", -- string used as prefix for truncated paths
-          padding = 1, -- left & right padding in number of spaces
-          hide_buffer_id = true,
-          devicons = {
-            enabled = true, -- enable or disable web dev icons
-            colored = true, -- enable color for web dev icons
-          },
-          highlight = {
-            current_buffer = "CybuFocus", -- current / selected buffer
-            adjacent_buffers = "CybuAdjacent", -- buffers not in focus
-            background = "CybuBackground", -- window background
-            border = "CybuBorder", -- border of the window
-          },
-        },
-      })
-      vim.keymap.set("n", "<S-TAB>", "<Plug>(CybuPrev)")
-      vim.keymap.set("n", "<TAB>", "<Plug>(CybuNext)")
-    end,
-  },
 }
