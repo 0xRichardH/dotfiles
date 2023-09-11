ad169834bd17ae7a2b9734ea24dcff741d9431aa
Richard Hao <richard@0xdev.dev>
2023-09-11 15:50:29 +0800

    feat(nvim): update rust-tools config

--- config/nvim/lua/plugins/rust.lua
+++ config/nvim/lua/plugins/rust.lua
@@ -1,7 +1,7 @@
 return {
   {
     "simrat39/rust-tools.nvim",
-    ft = "rust",
+    ft = { "rust", "rs" },
     opts = {
       reload_workspace_from_cargo_toml = true,
       server = {
@@ -24,13 +24,6 @@ return {
             typing = {
               autoClosingAngleBrackets = {
                 enable = true,
-              },
-            },
-            hover = {
-              actions = {
-                references = {
-                  enable = true,
-                },
               },
             },
             inlayHints = {
@@ -60,44 +53,63 @@ return {
         },
       },
       tools = {
+        inlay_hints = {
+          -- automatically set inlay hints (type hints)
+          -- default: true
+          auto = true,
+
+          -- Only show inlay hints for the current line
+          only_current_line = false,
+
+          -- whether to show parameter hints with the inlay hints or not
+          -- default: true
+          show_parameter_hints = true,
+
+          -- prefix for parameter hints
+          -- default: "<-"
+          parameter_hints_prefix = "<- ",
+
+          -- prefix for all the other hints (type, chaining)
+          -- default: "=>"
+          other_hints_prefix = "=> ",
+
+          -- whether to align to the length of the longest line in the file
+          max_len_align = false,
+
+          -- padding from the left if max_len_align is true
+          max_len_align_padding = 1,
+
+          -- whether to align to the extreme right or not
+          right_align = false,
+
+          -- padding from the right if right_align is true
+          right_align_padding = 7,
+
+          -- The color of the hints
+          highlight = "Comment",
+        },
         hover_actions = {
+          border = {
+            { "╭", "FloatBorder" },
+            { "─", "FloatBorder" },
+            { "╮", "FloatBorder" },
+            { "│", "FloatBorder" },
+            { "╯", "FloatBorder" },
+            { "─", "FloatBorder" },
+            { "╰", "FloatBorder" },
+            { "│", "FloatBorder" },
+          },
+          max_width = nil,
+          max_height = nil,
           auto_focus = true,
         },
-      },
-      inlay_hints = {
-        -- automatically set inlay hints (type hints)
-        -- default: true
-        auto = true,
-
-        -- Only show inlay hints for the current line
-        only_current_line = false,
-
-        -- whether to show parameter hints with the inlay hints or not
-        -- default: true
-        show_parameter_hints = true,
-
-        -- prefix for parameter hints
-        -- default: "<-"
-        parameter_hints_prefix = "<- ",
-
-        -- prefix for all the other hints (type, chaining)
-        -- default: "=>"
-        other_hints_prefix = "=> ",
-
-        -- whether to align to the length of the longest line in the file
-        max_len_align = false,
-
-        -- padding from the left if max_len_align is true
-        max_len_align_padding = 1,
-
-        -- whether to align to the extreme right or not
-        right_align = false,
-
-        -- padding from the right if right_align is true
-        right_align_padding = 7,
-
-        -- The color of the hints
-        highlight = "Comment",
+        dap = {
+          adapter = {
+            type = "executable",
+            command = "lldb-vscode",
+            name = "rt_lldb",
+          },
+        },
       },
     },
   },
