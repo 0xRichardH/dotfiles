5d05f93aba3e56031b64486a4f487bda834a1b08
Richard Hao <richard@0xdev.dev>
2023-09-13 23:49:15 +0800

    feat(nvim): Use <tab> for completion and snippets (supertab)
    https://www.lazyvim.org/configuration/recipes#supertab
    Signed-off-by: Richard Hao <richard@0xdev.dev>

---
+++ config/nvim/lua/plugins/luasnip.lua
@@ -0,0 +1,8 @@
+-- https://www.lazyvim.org/configuration/recipes#supertab
+-- Disable default <tab> and <s-tab> behavior in LuaSnip
+return {
+  "L3MON4D3/LuaSnip",
+  keys = function()
+    return {}
+  end,
+}
