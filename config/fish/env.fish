4664674da0ed6d842b762afa0fbb9d20102b7cc2
Richard Hao <richard@0xdev.dev>
2023-09-14 10:18:31 +0800

    chore: update fzf default options
    Signed-off-by: Richard Hao <richard@0xdev.dev>

--- config/fish/env.fish
+++ config/fish/env.fish
@@ -9,8 +9,16 @@ set -U LC_ALL en_US.UTF-8
 set -U TERM xterm-256color
 
 set -Ux FZF_DEFAULT_COMMAND "fd -H -E '.git'"
-set -Ux FZF_DEFAULT_OPTS "--reverse --no-info --pointer='' --marker='' \
---ansi --color='16,bg+:-1,gutter:-1,prompt:4,pointer:5,marker:6'"
+set -Ux FZF_DEFAULT_OPTS "\
+--exact \
+--reverse \
+--border rounded \
+--no-info \
+--pointer=' ' \
+--marker=' ' \
+--ansi \
+--color='16,bg+:-1,gutter:-1,prompt:5,pointer:5,marker:6,border:4,label:4,header:italic'"
+
 set -Ux FZF_TMUX_OPTS "-p 55%,60%"
 set -Ux FZF_CTRL_R_OPTS "--border-label=' History ' --prompt=' '"
 
