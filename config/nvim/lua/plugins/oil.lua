8e72f62c171aeb1a73d9d0255f1e182782ef4e40
Richard Hao <richard@0xdev.dev>
2023-09-18 13:54:11 +0800

    feat(nvim): use mini-files.nvim to replace `oil.nvim` and `lf.nvim`
    Signed-off-by: Richard Hao <richard@0xdev.dev>

--- config/nvim/lua/plugins/oil.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "stevearc/oil.nvim",
-  opts = {},
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  init = function()
-    vim.keymap.set("n", "-", require("oil").open, { desc = "Open parent directory" })
-  end,
-}
--- config/nvim/lua/plugins/toggleterm.lua
+++
@@ -1,1 +0,0 @@
-return { "akinsho/toggleterm.nvim", config = true }
