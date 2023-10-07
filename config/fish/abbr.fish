39c4e146d97f4974d5aa62b33dec4e93f7cad4cf
Richard Hao <richard@0xdev.dev>
2023-10-07 19:31:10 +0800

    chore(fish): organize abbr

--- config/fish/abbr.fish
+++ config/fish/abbr.fish
@@ -1,8 +1,25 @@
-abbr vim nvim
-abbr ls 'exa --git --icons --color=always --group-directories-first'
+abbr ba "bw_add_sshkeys -f ssh-agent -p passphrase -d"
+
 abbr cd z
-abbr psql pgcli
-abbr npm pnpm
-abbr v 'fd --type f --hidden --exclude .git | fzf-tmux -p --reverse | xargs lvim'
 abbr cl clear
+abbr cx "chmod +x"
+
+abbr dps "docker ps --format 'table {{.Names}}\t{{.Status}}'"
+abbr dc "docker compose"
+
+abbr ls 'exa --git --icons --color=always --group-directories-first'
+
 abbr mrsk kamal # kamal is former mrsk.
+
+abbr npm pnpm
+
+abbr psql pgcli
+
+abbr r "source ~/.config/fish/config.fish"
+
+abbr tr "tldr --list | fzf --header 'tldr (tealdeer)' --reverse --preview 'tldr {1} --color=always' --preview-window=right,80% | xargs tldr"
+
+abbr v "nvim +GoToFile"
+abbr vim nvim
+
+abbr x "chmod +x (ls | gum filter --limit 1 --header 'chmod +x')"
