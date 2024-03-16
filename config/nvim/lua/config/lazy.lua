db56bb8084867395749d3258b76cfa9b978a55e5
Richard Hao <richard@0xdev.dev>
2024-03-17 00:05:10 +0800

    chore(nvim): update plugins

--- config/nvim/lua/config/lazy.lua
+++ config/nvim/lua/config/lazy.lua
@@ -15,6 +15,7 @@ require("lazy").setup({
     -- { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.java" },
+    { import = "lazyvim.plugins.extras.lang.python" },
     -- { import = "lazyvim.plugins.extras.formatting.prettier" },
     -- { import = "lazyvim.plugins.extras.linting.eslint" },
     { import = "lazyvim.plugins.extras.coding.yanky" },
@@ -33,6 +34,7 @@ require("lazy").setup({
   ui = {
     border = "single",
   },
+  install = { colorscheme = { "rose-pine" } },
   checker = { enabled = false }, -- automatically check for plugin updates
   performance = {
     rtp = {
