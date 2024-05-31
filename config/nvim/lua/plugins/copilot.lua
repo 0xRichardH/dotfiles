ba0b513ba293a9b48db551b46c1eaf175f98a527
Richard Hao <richard@0xdev.dev>
2024-05-31 15:44:42 +0800

    feat(nvim): update copilot and add CopilotChat plugin
    This commit updates the configuration for the `copilot.lua` plugin and
    adds a new plugin `CopilotChat.nvim`. The `copilot.lua` configuration
    has been updated to be more readable and maintainable by wrapping it
    inside a table. The `CopilotChat.nvim` plugin has been added with its
    own configuration, including enabling debugging and specifying
    dependencies. This commit also updates the `lazy-lock.json` file to
    reflect the latest commits of various plugins.

--- config/nvim/lua/plugins/copilot.lua
+++ config/nvim/lua/plugins/copilot.lua
@@ -1,51 +1,153 @@
 return {
-  "zbirenbaum/copilot.lua",
-  cmd = "Copilot",
-  event = "InsertEnter",
-  config = function()
-    require("copilot").setup({
-      filetypes = {
-        ["*"] = true,
-      },
-      suggestion = {
-        enabled = true,
-        auto_trigger = true,
-        keymap = {
-          accept = "<C-a>",
-          dismiss = "<C-x>",
-          next = "<C-j>",
-          prev = "<C-k>",
+  {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    event = "InsertEnter",
+    config = function()
+      require("copilot").setup({
+        filetypes = {
+          ["*"] = true,
         },
-      },
-      panel = { enabled = false },
-    })
-  end,
-  keys = function()
-    return {
+        suggestion = {
+          enabled = true,
+          auto_trigger = true,
+          keymap = {
+            accept = "<C-a>",
+            dismiss = "<C-x>",
+            next = "<C-j>",
+            prev = "<C-k>",
+          },
+        },
+        panel = { enabled = false },
+      })
+    end,
+    keys = function()
+      return {
+        {
+          "<C-j>",
+          function()
+            return require("copilot.suggestion").next()
+          end,
+          mode = { "i" },
+          expr = true,
+        },
+        {
+          "<C-k>",
+          function()
+            return require("copilot.suggestion").prev()
+          end,
+          mode = { "i" },
+          expr = true,
+        },
+        {
+          "<C-x>",
+          function()
+            return require("copilot.suggestion").dismiss()
+          end,
+          mode = { "i" },
+          expr = true,
+        },
+      }
+    end,
+  },
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    branch = "canary",
+    dependencies = {
+      { "zbirenbaum/copilot.lua" }, -- or github/copilot.vim
+      { "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
+    },
+    opts = {
+      debug = true, -- Enable debugging
+      -- See Configuration section for rest
+    },
+    -- See Commands section for default commands if you want to lazy load on them
+    event = "VeryLazy",
+    keys = {
+      -- Show help actions with telescope
       {
-        "<C-j>",
+        "<leader>ah",
         function()
-          return require("copilot.suggestion").next()
+          local actions = require("CopilotChat.actions")
+          require("CopilotChat.integrations.telescope").pick(actions.help_actions())
         end,
-        mode = { "i" },
-        expr = true,
+        desc = "CopilotChat - Help actions",
+      },
+      -- Show prompts actions with telescope
+      {
+        "<leader>ap",
+        function()
+          local actions = require("CopilotChat.actions")
+          require("CopilotChat.integrations.telescope").pick(actions.prompt_actions())
+        end,
+        desc = "CopilotChat - Prompt actions",
       },
       {
-        "<C-k>",
-        function()
-          return require("copilot.suggestion").prev()
-        end,
-        mode = { "i" },
-        expr = true,
+        "<leader>ap",
+        ":lua require('CopilotChat.integrations.telescope').pick(require('CopilotChat.actions').prompt_actions({selection = require('CopilotChat.select').visual}))<CR>",
+        mode = "x",
+        desc = "CopilotChat - Prompt actions",
+      },
+      -- Code related commands
+      { "<leader>ae", "<cmd>CopilotChatExplain<cr>", desc = "CopilotChat - Explain code" },
+      { "<leader>at", "<cmd>CopilotChatTests<cr>", desc = "CopilotChat - Generate tests" },
+      { "<leader>ar", "<cmd>CopilotChatReview<cr>", desc = "CopilotChat - Review code" },
+      { "<leader>aR", "<cmd>CopilotChatRefactor<cr>", desc = "CopilotChat - Refactor code" },
+      { "<leader>an", "<cmd>CopilotChatBetterNamings<cr>", desc = "CopilotChat - Better Naming" },
+      -- Chat with Copilot in visual mode
+      {
+        "<leader>av",
+        ":CopilotChatVisual",
+        mode = "x",
+        desc = "CopilotChat - Open in vertical split",
       },
       {
-        "<C-x>",
-        function()
-          return require("copilot.suggestion").dismiss()
-        end,
-        mode = { "i" },
-        expr = true,
+        "<leader>ax",
+        ":CopilotChatInline<cr>",
+        mode = "x",
+        desc = "CopilotChat - Inline chat",
       },
-    }
-  end,
+      -- Custom input for CopilotChat
+      {
+        "<leader>ai",
+        function()
+          local input = vim.fn.input("Ask Copilot: ")
+          if input ~= "" then
+            vim.cmd("CopilotChat " .. input)
+          end
+        end,
+        desc = "CopilotChat - Ask input",
+      },
+      -- Generate commit message based on the git diff
+      {
+        "<leader>am",
+        "<cmd>CopilotChatCommit<cr>",
+        desc = "CopilotChat - Generate commit message for all changes",
+      },
+      {
+        "<leader>aM",
+        "<cmd>CopilotChatCommitStaged<cr>",
+        desc = "CopilotChat - Generate commit message for staged changes",
+      },
+      -- Quick chat with Copilot
+      {
+        "<leader>aq",
+        function()
+          local input = vim.fn.input("Quick Chat: ")
+          if input ~= "" then
+            vim.cmd("CopilotChatBuffer " .. input)
+          end
+        end,
+        desc = "CopilotChat - Quick chat",
+      },
+      -- Debug
+      { "<leader>ad", "<cmd>CopilotChatDebugInfo<cr>", desc = "CopilotChat - Debug Info" },
+      -- Fix the issue with diagnostic
+      { "<leader>af", "<cmd>CopilotChatFixDiagnostic<cr>", desc = "CopilotChat - Fix Diagnostic" },
+      -- Clear buffer and chat history
+      { "<leader>al", "<cmd>CopilotChatReset<cr>", desc = "CopilotChat - Clear buffer and chat history" },
+      -- Toggle Copilot Chat Vsplit
+      { "<leader>av", "<cmd>CopilotChatToggle<cr>", desc = "CopilotChat - Toggle" },
+    },
+  },
 }
