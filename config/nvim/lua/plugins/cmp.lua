28e3cce589226a300205ce44899f448fb2a393d2
Richard Hao <richard@0xdev.dev>
2024-07-14 16:23:31 +0800

    feat(nvim): update cmp configs

--- config/nvim/lua/plugins/cmp.lua
+++ config/nvim/lua/plugins/cmp.lua
@@ -2,59 +2,71 @@ return {
   {
     "hrsh7th/nvim-cmp",
     opts = function(_, opts)
-      local has_words_before = function()
-        unpack = unpack or table.unpack
-        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-        return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-      end
-
       local cmp = require("cmp")
-      local luasnip = require("luasnip")
-
       -- add boarder to cmp window
       local cmp_window = cmp.config.window
       opts.window = {
         completion = cmp_window.bordered(),
-        documentation = false,
+        -- documentation = false,
       }
-
-      -- disable ghost text
-      opts.experimental = {
-        ghost_text = false,
-      }
-      -- opts.experimental = {
-      --   ghost_text = {
-      --     hl_group = "CmpGhostText",
-      --   },
-      -- }
-
-      -- Setup supertab in cmp
-      -- https://www.lazyvim.org/configuration/recipes#supertab
-      opts.mapping = vim.tbl_extend("force", opts.mapping, {
-        ["<Tab>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            -- You could replace select_next_item() with confirm({ select = true }) to get VS Code autocompletion behavior
-            cmp.select_next_item()
-          -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
-          -- this way you will only jump inside the snippet region
-          elseif luasnip.expand_or_locally_jumpable() then
-            luasnip.expand_or_jump()
-          elseif has_words_before() then
-            cmp.complete()
-          else
-            fallback()
-          end
-        end, { "i", "s" }),
-        ["<S-Tab>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_prev_item()
-          elseif luasnip.jumpable(-1) then
-            luasnip.jump(-1)
-          else
-            fallback()
-          end
-        end, { "i", "s" }),
-      })
     end,
   },
+  -- {
+  --   "hrsh7th/nvim-cmp",
+  --   opts = function(_, opts)
+  --     local has_words_before = function()
+  --       unpack = unpack or table.unpack
+  --       local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  --       return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+  --     end
+  --
+  --     local cmp = require("cmp")
+  --     local luasnip = require("luasnip")
+  --
+  --     -- add boarder to cmp window
+  --     local cmp_window = cmp.config.window
+  --     opts.window = {
+  --       completion = cmp_window.bordered(),
+  --       documentation = false,
+  --     }
+  --
+  --     -- disable ghost text
+  --     opts.experimental = {
+  --       ghost_text = false,
+  --     }
+  --     -- opts.experimental = {
+  --     --   ghost_text = {
+  --     --     hl_group = "CmpGhostText",
+  --     --   },
+  --     -- }
+  --
+  --     -- Setup supertab in cmp
+  --     -- https://www.lazyvim.org/configuration/recipes#supertab
+  --     opts.mapping = vim.tbl_extend("force", opts.mapping, {
+  --       ["<Tab>"] = cmp.mapping(function(fallback)
+  --         if cmp.visible() then
+  --           -- You could replace select_next_item() with confirm({ select = true }) to get VS Code autocompletion behavior
+  --           cmp.select_next_item()
+  --         -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
+  --         -- this way you will only jump inside the snippet region
+  --         elseif luasnip.expand_or_locally_jumpable() then
+  --           luasnip.expand_or_jump()
+  --         elseif has_words_before() then
+  --           cmp.complete()
+  --         else
+  --           fallback()
+  --         end
+  --       end, { "i", "s" }),
+  --       ["<S-Tab>"] = cmp.mapping(function(fallback)
+  --         if cmp.visible() then
+  --           cmp.select_prev_item()
+  --         elseif luasnip.jumpable(-1) then
+  --           luasnip.jump(-1)
+  --         else
+  --           fallback()
+  --         end
+  --       end, { "i", "s" }),
+  --     })
+  --   end,
+  -- },
 }
